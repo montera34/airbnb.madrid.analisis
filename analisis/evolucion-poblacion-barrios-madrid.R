ff9bf7a1552819e532119161defcdc726cf86a28
numeroteca <pablo@basurama.org>
2018-05-27 01:23:44 +0200

    añade datos demográficos madrid

--- analisis/evolucion-poblacion-barrios-madrid.R
+++ analisis/evolucion-poblacion-barrios-madrid.R
@@ -1,18 +1,20 @@
-evolucion <- read.delim("data/output/demografia/madrid_censo-poblacion-por-barrios_evolucion-2010-2018_base-100-es-2010.csv",sep = ",")
-
 library(reshape)
+library(tidyverse)
 
+#-------- visualize evolution index 100---------
+evolucion <- read.delim("data/output/demografia/madrid_censo-poblacion-por-barrios_evolucion-2010-2018_base-100-es-2015.csv",sep = ",")
+
+# reshape data to long format to prepare to plot time series timeline
 m <- reshape(evolucion, direction = "long", varying = list(names(evolucion)[5:13]), v.names = "Value", 
                idvar = c("nombre_barrio"), timevar = "Year", times = 2010:2018)
 
-
-
+# The "almendra central" lines and points are plotted for a second time later to bediplayed on top
 ggplot() +
   geom_point(data=m,aes(x = Year, y = Value, color = Ubicación)) + 
   geom_line(data=m,aes(x = Year, y = Value, color = Ubicación, group=nombre_barrio)) + 
   geom_point(data=m[m$Ubicación=="Almendra central",],aes(x = Year, y = Value, color = Ubicación)) + 
   geom_line(data=m[m$Ubicación=="Almendra central",],aes(x = Year, y = Value, color = Ubicación, group=nombre_barrio)) + 
-  coord_cartesian(ylim = c(70, 170)) +
+  coord_cartesian(ylim = c(70, 125)) +
   labs(title = "Evolución población por barrio. Madrid.") +
   ylab("100 = año 2010") + xlab("Años") + labs(colour = "Ubicación") +
   theme(axis.text.y = element_text(size=12),
@@ -26,5 +28,4 @@ ggplot() +
         panel.grid.major.y = element_line( size=.1, color="grey" ),
         legend.position = "bottom",
         legend.text = element_text(size=15)
-  ) 
-  
+  )
